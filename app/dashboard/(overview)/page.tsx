262c5a980617b297b73e91884f5ae1f310bb15ba
vishalicious213 <vishalicious213@gmail.com>
2024-02-06 21:49:13 -0800

    updated /app/dashboard/(overview)/page.tsx to use Suspense to render RevenueChart. updated/app/ui/dashboard/revenue-chart.tsx to fetch its own data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,10 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart'
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices'
 import { lusitana } from '@/app/ui/fonts'
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+// import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data'
+import { Suspense } from "react"
+import { RevenueChartSkeleton } from "@/app/ui/skeletons"
 
 export default async function Page() {
-    const revenue = await fetchRevenue()
+    // const revenue = await fetchRevenue()
     const latestInvoices = await fetchLatestInvoices()
     const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = await fetchCardData()
 
@@ -21,7 +24,10 @@ export default async function Page() {
                 <Card title="Total Customers" value={numberOfCustomers} type="customers" />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue}  />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                {/* <RevenueChart revenue={revenue}  /> */}
                 <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
